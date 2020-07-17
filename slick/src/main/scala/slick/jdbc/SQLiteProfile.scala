995ec4072ca599b08a5d30c8c43e124638ce25e1
Stefan Zeiger <szeiger@novocode.com>
2020-07-17 23:30:25 +0200

    Compile on Dotty
    Slick itself compiles, the tests do not yet. Macros still missing.

--- slick/src/main/scala/slick/jdbc/SQLiteProfile.scala
+++ slick/src/main/scala/slick/jdbc/SQLiteProfile.scala
@@ -97,7 +97,7 @@ trait SQLiteProfile extends JdbcProfile {
     - JdbcCapabilities.returnMultipleInsertKey
   )
 
-  class ModelBuilder(mTables: Seq[MTable], ignoreInvalidDefaults: Boolean)(implicit ec: ExecutionContext) extends JdbcModelBuilder(mTables, ignoreInvalidDefaults) {
+  class SQLiteModelBuilder(mTables: Seq[MTable], ignoreInvalidDefaults: Boolean)(implicit ec: ExecutionContext) extends JdbcModelBuilder(mTables, ignoreInvalidDefaults) {
 
     override def createColumnBuilder(tableBuilder: TableBuilder, meta: MColumn): ColumnBuilder = new SQLiteColumnBuilder(tableBuilder, meta)
     override def createPrimaryKeyBuilder(tableBuilder: TableBuilder, meta: Seq[MPrimaryKey]): PrimaryKeyBuilder = new SQLitePrimaryKeyBuilder(tableBuilder, meta)
@@ -120,7 +120,7 @@ trait SQLiteProfile extends JdbcProfile {
       override def varying = dbType == Some("VARCHAR")
       override def default: Option[Option[Any]] = meta.columnDef.map((_,tpe)).collect{
         case ("null",_)  => Some(None) // 3.7.15-M1
-        case (v , "java.sql.Timestamp") => {
+        case (v, "java.sql.Timestamp") => {
           import scala.util.{Try, Success}
           val convertors = Seq((s: String) => new java.sql.Timestamp(s.toLong),
             (s: String) => java.sql.Timestamp.valueOf(s),
@@ -136,9 +136,9 @@ trait SQLiteProfile extends JdbcProfile {
             }
           )
           val v2 = v.replace("\"", "")
-          convertors.collectFirst(fn => Try(fn(v2)) match{
+          convertors.iterator.map(fn => Try(fn(v2))).collectFirst {
             case Success(v) => Some(v)
-          })
+          }
         }
       }.getOrElse{super.default}
       override def tpe = dbType match {
@@ -164,7 +164,7 @@ trait SQLiteProfile extends JdbcProfile {
   }
 
   override def createModelBuilder(tables: Seq[MTable], ignoreInvalidDefaults: Boolean)(implicit ec: ExecutionContext): JdbcModelBuilder =
-    new ModelBuilder(tables, ignoreInvalidDefaults)
+    new SQLiteModelBuilder(tables, ignoreInvalidDefaults)
 
   override def defaultTables(implicit ec: ExecutionContext): DBIO[Seq[MTable]] =
     MTable.getTables(Some(""), Some(""), None, Some(Seq("TABLE")))
